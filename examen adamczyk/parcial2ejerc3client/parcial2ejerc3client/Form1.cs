87299911d204412c063144c2b4c8b13e43b07740
FrancoAdamczyk <adamczyk.franco@gmail.com>
2019-05-18 09:48:03 -0400

    Examen2

---
+++ examen adamczyk/parcial2ejerc3client/parcial2ejerc3client/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace parcial2ejerc3client
+{
+    public partial class Form1 : Form
+    {
+
+        string aux = "";
+        const String rutaestado0 = "C:\\Users\\Estudiante\\programacionIII\\Ahorcado\\0.png";
+        const String rutaestado1 = "C:\\Users\\Estudiante\\programacionIII\\Ahorcado\\1.png";
+        const String rutaestado2 = "C:\\Users\\Estudiante\\programacionIII\\Ahorcado\\2.png";
+        const String rutaestado3 = "C:\\Users\\Estudiante\\programacionIII\\Ahorcado\\3.png";
+        const String rutaestado4 = "C:\\Users\\Estudiante\\programacionIII\\Ahorcado\\4.png";
+        const String rutaestado5 = "C:\\Users\\Estudiante\\programacionIII\\Ahorcado\\5.png";
+        const String rutaestado6 = "C:\\Users\\Estudiante\\programacionIII\\Ahorcado\\6.png";
+        const String rutaestado7 = "C:\\Users\\Estudiante\\programacionIII\\Ahorcado\\7.png";
+        
+
+        String Solicitud = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Renderizar(String mensaje)
+        {
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ahorcado.getInstancia().Iniciar();
+        }
+
+        private void txtLetra_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void txtPalabra_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblResultado.Text = ahorcado.getInstancia().Buscar(txtLetra.Text[0]);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ examen adamczyk/parcial2ejerc3serv/parcial2ejerc3serv/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace parcial2ejerc3serv
+{
+    class Program
+    {
+        string data = "";
+        static void Main(string[] args)
+        {
+            byte[] bytes = new Byte[1024];
+
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 11000);
+            Socket listener = new Socket(IPAddress.Any.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+            String Respuesta = "";
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+                while (true)
+                {
+                    Console.WriteLine("Esperando por conexiones ...");
+                    Socket handler = listener.Accept();
+                    // La conexion de entrada necesita ser procesada.  
+                    int bytesRec = handler.Receive(bytes);
+                    string solicitud = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                    if (solicitud == "Iniciar")
+                    {
+                       ahorcado.getInstancia().Iniciar();
+                        Respuesta = "ok";
+                    }
+                    else if (solicitud == "BuscarPalabra")
+                        Respuesta = ahorcado.getInstancia().BuscarPalabra();
+                    else
+                    {
+                        String[] sol = solicitud.Split(',');
+                        
+                        Respuesta = ahorcado.getInstancia().BuscarPalabra();
+                    }
+                    byte[] msg = Encoding.ASCII.GetBytes(Respuesta);
+                    handler.Send(msg);
+                    Console.WriteLine("Texto recibido: {0}", solicitud);
+                    Console.WriteLine("Texto enviado: {0}", Respuesta);
+
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("\nPresiones ENTER para continuar...");
+            Console.Read();
+
+
+        }
+    }
+}
+
