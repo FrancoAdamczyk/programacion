54394ddb7f3a8c4378796fb502a17afe1b2be268
FrancoAdamczyk <adamczyk.franco@gmail.com>
2019-05-18 09:28:32 -0400

    Examen2

---
+++ examen adamczyk/parcial2ejerc1/parcial2ejerc1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace parcial2ejerc1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double x;
+            x = Convert.ToDouble(textBox1.Text);
+            clima cl = clima.getInstance();
+            cl.colocartemperatura(Convert.ToDouble(textBox1.Text));
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clima cl = clima.getInstance();
+            label1.Text = cl.obtenertemp();
+        }
+    }
+}
---
+++ examen adamczyk/parcial2ejerc1/parcial2ejerc1/clima.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parcial2ejerc1
+{
+    class clima
+    {
+        private double temperatura = 0;
+        private static clima instance;
+        private clima()
+        {
+
+        }
+        public void colocartemperatura(double temp)
+        {
+
+            temperatura = temp;
+        }
+       
+       
+        public static clima getInstance()
+        {
+            if (instance == null)
+                instance = new clima();
+
+            return instance;
+        }
+        public string obtenertemp()
+        {
+            return temperatura.ToString();
+        }
+    }
+}
